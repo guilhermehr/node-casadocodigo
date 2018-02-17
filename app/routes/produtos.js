13eeee0023c293490c2e4626aab8eccfca664073
Guilherme <guilhermehr@gmail.com>
2018-02-16 23:29:45 -0200

    Isolando o acesso aos dados do banco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,12 +1,15 @@
-module.exports = function(app) {
+module.exports = function (app) {
     app.get('/produtos', function (req, res) {
         //    console.log("Atendendo a requisição...");
         //    console.log("listando...");
 
         var connection = app.infra.connectionFactory();
 
-        connection.query('select * from livros', function(err,results){
-            //console.log(err);
+        var produtosBanco = app.infra.produtosBanco;
+
+        produtosBanco.lista(connection, function (err, results) {
+            console.log(err);
+            console.log(results);
             res.render('produtos/lista', {lista:results});
         });
 
