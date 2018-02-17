568523a55c99ff85e559ce0e602fe17e570297e9
Guilherme <guilhermehr@gmail.com>
2018-02-17 14:19:40 -0200

    Salvando produtos no banco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -7,16 +7,31 @@ module.exports = function (app) {
 
         // new cria um novo contexto, novo objeto para this, senão referencia o contexto
         // de quem criou, nesse caso o express
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function (err, results) {
+        produtosDAO.lista(function (err, results) {
             console.log(err);
             console.log(results);
             res.render('produtos/lista', {lista:results});
         });
-
         connection.end();
+    });
 
+    app.get('/produtos/form', function(req, res) {
+        //console.log("cheguei aqui");
+        res.render('produtos/form');
+    });
+
+    app.post('/produtos/salva', function(req, res) {
+
+        var produto = req.body;
+        console.log(produto);
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function(err, resultados) {
+            res.render('produtos/lista');
+        });
     });
 }
 
--- config/express.js
+++ config/express.js
@@ -5,6 +5,7 @@ versão sempre será criada.
 */
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function () {
 
@@ -12,6 +13,8 @@ module.exports = function () {
 
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+
+    app.use(bodyParser.urlencoded({extended: true}));
 
     load('routes', { cwd: 'app' })
         .then('infra')
