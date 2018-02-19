bb4781356c28cf9915965947c1ca958d155b53e8
Guilherme <guilhermehr@gmail.com>
2018-02-18 21:41:36 -0300

    Melhorando as verificações de erro e acerto

--- test/produtos.js
+++ test/produtos.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var assert = require('assert');
 
 describe('#ProdutosController', function() {
     it('#listagem json', function(done) {
@@ -13,16 +14,9 @@ describe('#ProdutosController', function() {
         };
 
         http.get(configuracoes, function(res) {
-            if(res.statusCode == 200) {
-                console.log("Status tá OK");
-            }
-            //console.log(res.headers['content-type']);
-            if(res.headers['content-type'] == 'application/json; charset=utf-8') {
-                console.log("Content-type tá OK");
-            }
+            assert.equal(res.statusCode, 302);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
             done();
         });
-
-        console.log("teste de verificação de listagem json");
     });
 });
