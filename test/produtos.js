9aad44e1eb4be887dbe45cbeb54563970729467a
Guilherme <guilhermehr@gmail.com>
2018-02-18 21:37:25 -0300

    Testando a listagem de produtos

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,28 @@
+var http = require('http');
+
 describe('#ProdutosController', function() {
-    it('#listagem json', function() {
+    it('#listagem json', function(done) {
+
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+
+        http.get(configuracoes, function(res) {
+            if(res.statusCode == 200) {
+                console.log("Status tá OK");
+            }
+            //console.log(res.headers['content-type']);
+            if(res.headers['content-type'] == 'application/json; charset=utf-8') {
+                console.log("Content-type tá OK");
+            }
+            done();
+        });
+
         console.log("teste de verificação de listagem json");
     });
 });
