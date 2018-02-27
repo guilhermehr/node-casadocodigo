4fd45a9093c3281ad18350600ec92be6ebbdfa18
Guilherme <guilhermehr@gmail.com>
2018-02-27 11:47:54 -0300

    Middlewares

--- config/express.js
+++ config/express.js
@@ -17,13 +17,27 @@ module.exports = function () {
     app.set('views', './app/views');
 
     // funções middleware que podem alterar o request
-    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(expressValidator());
 
     load('routes', { cwd: 'app' })
         .then('infra')
         .into(app);
+
+    app.use(function (req, res, next) {
+        res.status(404).render('erros/404');
+        next();
+    });
+
+    app.use(function (error, req, res, next) {
+
+        if(process.env.NODE_ENV == 'production') {
+            res.status(500).render('erros/500');
+            return;
+        }
+        next(error);
+    });
 
     return app;
 }
